5a26b5c93a04aeffa3739025a405178d1e0a9b89
Oğuz KABA <73114283+oguzkaba@users.noreply.github.com>
2021-01-11 00:00:50 +0300

    commit32

--- lib/views/tablet/widgets/tabletnav_widget2.dart
+++ lib/views/tablet/widgets/tabletnav_widget2.dart
@@ -32,15 +32,25 @@ class TabNavWidget extends StatelessWidget {
     ThemeController tc = Get.put(ThemeController());
     return Obx(
       () => Container(
-        width: 70,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(.2),
+                blurRadius: 2,
+                offset: Offset(0.5, 0.0),
+                spreadRadius: 2)
+          ],
+        ),
+        width: 75,
         height: Get.height,
         child: NavigationRail(
+          labelType: NavigationRailLabelType.selected,
           groupAlignment: 0,
           onDestinationSelected: (int index) {
             pc.pindex = index;
           },
-          leading: Image.asset("assets/images/light2.png"),
-          trailing: _logoutButton(sc),
+          leading: _leadingwidget(sc),
           destinations: [
             for (var i = 0; i < navIcon.length; i++)
               NavigationRailDestination(
@@ -49,31 +59,59 @@ class TabNavWidget extends StatelessWidget {
                   selectedIcon: Icon(navIcon[i], color: Global.focusedBlue)),
           ],
           selectedIndex: pc.pindex,
+          trailing: _logoutButton(sc),
         ),
       ),
     );
   }
 }
 
+Widget _leadingwidget(SharedPrefController sc) {
+  return Padding(
+    padding: const EdgeInsets.only(top:20),
+    child: Column(
+      children: [
+        CircleAvatar(
+            backgroundColor: Global.dark_red,
+            radius: 30,
+            child: Icon(Icons.people, size: 40, color: Global.white)),
+        SizedBox(
+          height: 10,
+        ),
+        Text(sc.uname, style: TextStyle(fontWeight: FontWeight.bold))
+      ],
+    ),
+  );
+}
+
 Widget _logoutButton(SharedPrefController sc) {
-  return InkWell(
-    onTap: () => Get.dialog(
-        ShowDialogWidget(
-            title: 'Logout',
-            tbtn1: 'OK',
-            tbtn2: 'CANCEL',
-            text1: 'Do you check out the account?',
-            text2: 'Account preferences will be reset!',
-            onPressed: () {
-              sc.isLogin = false;
-              Get.offNamed('t/loginPage');
-              print('Çıkış yapıldı: ' + sc.uname);
-            }),
-        transitionDuration: Duration(milliseconds: 300),
-        barrierDismissible: false),
-    child: Icon(
-      Icons.exit_to_app,
-      color: Global.dark_red,
+  return Padding(
+    padding: const EdgeInsets.only(top: 60,bottom: 10),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        InkWell(
+          onTap: () => Get.dialog(
+              ShowDialogWidget(
+                  title: 'Logout',
+                  tbtn1: 'OK',
+                  tbtn2: 'CANCEL',
+                  text1: 'Do you check out the account?',
+                  text2: 'Account preferences will be reset!',
+                  onPressed: () {
+                    sc.isLogin = false;
+                    Get.offNamed('t/loginPage');
+                    print('Çıkış yapıldı: ' + sc.uname);
+                  }),
+              transitionDuration: Duration(milliseconds: 300),
+              barrierDismissible: false),
+          child: Icon(
+            Icons.exit_to_app,
+            color: Global.dark_red,
+          ),
+        ),
+        Text('Logout', style: TextStyle(fontWeight: FontWeight.bold))
+      ],
     ),
   );
 }
